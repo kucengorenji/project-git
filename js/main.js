ce3a1cd6911aad9713fdb4031dcdb3f024ab2b9d
kucengorenji <mikelrisyad@gmail.com>
2021-03-20 13:25:33 +0700

    add the todo list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-fetch()
+let target = document.querySelector(".todo_lists");
+target.innerHTML = "Loading..."
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => {
+        return res.json()
+    })
+    .then(todos => {
+        let html;
+        todos.forEach(todo => {
+            html += `
+            <div class="card my-4">
+                <div class="card-body ${todo.completed ? "done" : "undone"}">
+                <h5 class="card-title">Title: ${todo.title}</h5>
+                <p class="card-text">
+                    Status ${todo.completed ? "Selesai" : "Belum selesai" }
+                </p>
+                </div>
+            </div>
+            `
+        })
+        target.innerHTML = html;
+    })
